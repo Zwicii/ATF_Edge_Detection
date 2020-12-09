ba854318e5e476063a4b7974a9e3204a5bf84a1f
Zwicii <vicioberascher@gmail.com>
2020-12-09 13:59:22 +0100

    add update angle and throttle

--- edgeDetection/simulator_communication_template.py
+++ edgeDetection/simulator_communication_template.py
@@ -10,16 +10,45 @@ app = Flask(__name__)
 socket_io = SocketIO(app)
 
 
+throttle = 0
+angle = 0
+step = -0.1
+COUNTER = 0
+
+def updateAngelAndThrottle(speed):
+    global throttle
+    global angle
+    global step
+
+    if speed >= 15:
+        throttle = 0
+    elif speed >= 10:
+        throttle = 0.5
+    else:
+        throttle = 1
+
+    angle += step
+    if angle >= 1:
+        angle = 0
+        step = -0.1
+    elif angle <= -1:
+        angle = 0
+        step = 0.1
+
+
+
 @socket_io.on('telemetry')
 def telemetry(data):
+    global COUNTER
     if data:
         speed = float(data["speed"])
-        image = Image.open(BytesIO(base64.b64decode(data["image"])))
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-        # TODO: calculate steering angle and throttle from image and speed
+        if COUNTER % 2 == 0:
+            updateAngelAndThrottle(speed)
 
-        send_control(steering_angle, throttle)
+        send_control(angle, throttle)
+
+        COUNTER = COUNTER + 1
     else:
         socket_io.emit('manual', data={}, skip_sid=True)
 
